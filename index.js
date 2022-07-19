5ff9bf848431e9cccc8896ffc88aceadc32e7784
Allison Kim <akeem159@yahoo.com>
2022-07-19 12:19:41 -0400

    commit tree 77231ccf5a97e96a552795796726348285af8774

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(fun = 'roller-skate') {
+    return `This Saturday, I want to ${fun}!`;
+}
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = `"*"`) {
+    const innerFunction = function (someValue = "special") {
+        return `You are ${flair + someValue + flair}!`
+    }
+    return innerFunction
+}
